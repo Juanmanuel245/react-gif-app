d4f4d8610b145e866fddf04d771c8f60883a333c
Juan Manuel Caffi <juanmanuelcaffi@gmail.com>
2020-09-26 12:56:13 -0300

    Docs agregado

---
+++ docs/precache-manifest.b9f3ccb8f7a6d53d7c2f9b1f8f7bb398.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1d4599563bdef3b723e7d7d62e4e4df8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "46c0c7901f810c063bbf",
+    "url": "/static/css/main.98a0fea8.chunk.css"
+  },
+  {
+    "revision": "1094a9d0787669c6640b",
+    "url": "/static/js/2.ba604ae7.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.ba604ae7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "46c0c7901f810c063bbf",
+    "url": "/static/js/main.4536d947.chunk.js"
+  },
+  {
+    "revision": "daee87f482498cdf724c",
+    "url": "/static/js/runtime-main.29242b1d.js"
+  }
+]);
